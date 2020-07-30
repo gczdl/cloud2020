b1045a1dc10940f78cb54f5a39084296f54f4b07
gczdl <1521088619@qq.com>
2020-07-30 17:52:26 +0800

    zookeeper

---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/PaymentMain8004.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zdl
+ * @create 2020/7/30 14:15
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class,args);
+    }
+
+}
