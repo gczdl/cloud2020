4e6d6ee4d29e5bb7ef61d03d32da257689609434
gczdl <1521088619@qq.com>
2020-08-02 21:38:31 +0800

    rm .idea

---
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
@@ -0,0 +1,10 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class OrderMain80 {
+
+}
